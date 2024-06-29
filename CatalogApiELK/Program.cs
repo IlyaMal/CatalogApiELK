92ed3f777d5ccc1ecb070dcaeb76ad19ef2ba21c
Илья Малютин <ilamalutin4@gmail.com>
2024-06-29 22:03:26 +0300

    initial

---
+++ CatalogApiELK/Program.cs
@@ -0,0 +1,94 @@
+using System.Reflection;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+
+namespace CatalogApiELK
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Configure logging first
+            ConfigureLogging();
+
+            // Create the builder
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container
+            ConfigureServices(builder.Services);
+
+            // Build the app
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline
+            Configure(app);
+
+            // Run the app
+            try
+            {
+                Log.Information("Starting web host");
+                app.Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        private static void ConfigureLogging()
+        {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{environment}.json", optional: true)
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .Enrich.WithMachineName()
+                .WriteTo.Debug()
+                .WriteTo.Console()
+                .WriteTo.Elasticsearch(ConfigureElasticSink(configuration, environment))
+                .Enrich.WithProperty("Environment", environment)
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+        }
+
+        private static ElasticsearchSinkOptions ConfigureElasticSink(IConfiguration configuration, string environment)
+        {
+            return new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:Uri"]))
+            {
+                AutoRegisterTemplate = true,
+                IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
+            };
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthorization();
+            services.AddControllers();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        private static void Configure(WebApplication app)
+        {
+            // Configure the HTTP request pipeline
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+        }
+    }
+}
